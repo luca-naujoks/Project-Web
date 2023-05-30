541ec09ff0d71843e6d66346c3bde137098513f1
Luca Hendrik Helms <lucahendrik.dev@gmail.com>
2023-05-30 20:59:10 +0200

    remove comment

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    // const data = JSON.stringify();
     res.status(200).json({ data: req.body });
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
